baf362ed32b21241d57cbb316fa201c9bcfc6a3e
aminurrahman4078 <aminurrahman9793@gmail.com>
2025-11-13 07:13:35 +0600

    Added verify firebase token and firebase services key

--- index.js
+++ index.js
@@ -6,9 +6,37 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// firebase service key
+const admin = require("firebase-admin");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyFirebaseToken = async (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1]
+    try {
+        const decoded = await admin.auth().verifyIdToken(token)
+        req.token_email = decoded.email
+        next()
+    } catch (error) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+}
+
+
+
 
 app.get('/', (req, res) => {
   res.send('plateshare server');
@@ -27,7 +55,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const database = client.db("PlateShare");
     const foodsCollection = database.collection("foods");
     const foodsRequestCollection = database.collection('food-request')
@@ -68,14 +96,14 @@ async function run() {
     })
 
     // post add food
-    app.post('/add-food', async (req, res) => {
+    app.post('/add-food',verifyFirebaseToken, async (req, res) => {
       const newFood = req.body
       const result = await foodsCollection.insertOne(newFood)
       res.send(result)
     })
 
     // update add food
-    app.patch('/add-food/:id', async (req, res) => {
+    app.patch('/add-food/:id',verifyFirebaseToken, async (req, res) => {
       const id = req.params.id;
       const updateFood = req.body;
       const query = { _id: new ObjectId(id) };
@@ -96,7 +124,7 @@ async function run() {
 
 
     // delete add food
-    app.delete('/add-food/:id', async (req, res) => {
+    app.delete('/add-food/:id',verifyFirebaseToken, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await foodsCollection.deleteOne(query)
@@ -114,14 +142,14 @@ async function run() {
     });
 
     // food request post
-    app.post('/food-request', async (req, res) => {
+    app.post('/food-request',verifyFirebaseToken, async (req, res) => {
       const newRequest = req.body
       const result = await foodsRequestCollection.insertOne(newRequest)
       res.send(result)
     })
 
     // Delete a food request
-    app.delete('/food-request/:id', async (req, res) => {
+    app.delete('/food-request/:id',verifyFirebaseToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await foodsRequestCollection.deleteOne(query);
@@ -130,7 +158,7 @@ async function run() {
 
     
     // Accept / Reject a food request
-    app.patch('/food-request/:id/status', async (req, res) => {
+    app.patch('/food-request/:id/status',verifyFirebaseToken, async (req, res) => {
       const { id } = req.params;
       const { status } = req.body; // 'accepted' or 'rejected'
       const query = { _id: new ObjectId(id) };
@@ -155,8 +183,8 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("✅ Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("✅ Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
     console.error("❌ MongoDB connection failed:", err);
   }
